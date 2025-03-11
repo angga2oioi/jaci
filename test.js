a70233dde0f143e2f866331ec989dc5626098653
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2025-03-11 13:22:19 +0700

    add select options

--- test.js
+++ test.js
@@ -1,35 +1,44 @@
 const jaci = require("./index");
-jaci.string("string : ",{required:false})
-.then((res)=>{
-    console.log(res);
-    return jaci.password("password :")
-})
-.then((res)=>{
-    console.log(res);
-    return jaci.alpha("alpha :")
-})
-.then((res)=>{
-    console.log(res);
-    return jaci.alphanum("alphanum :")
-})
-.then((res)=>{
-    console.log(res);
-    return jaci.number("number :")
-})
-.then((res)=>{
-    console.log(res);
-    return jaci.regex("regex:",{regex:/^[A-Za-z]+$/})
-})
-.then((res)=>{
-    console.log(res);
-    return jaci.confirm("confirm (Yea/Nay):",{confirm:{true:"Y",false:"N"}})
-})
-.then((res)=>{
-    console.log(res);
-    jaci.done();
-    return;
-})
-.catch((e)=>{
-    console.log("error",e);
-    jaci.done();
-})
+jaci.string("string : ", { required: false })
+    
+    .then((res) => {
+        console.log(res);
+        return jaci.password("password :")
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.alpha("alpha :")
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.alphanum("alphanum :")
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.number("number :")
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.regex("regex:", { regex: /^[A-Za-z]+$/ })
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.confirm("confirm (Yea/Nay):", { confirm: { true: "Y", false: "N" } })
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.select("What do you like ", { options: { rabbit: "R", cat: "C" } })
+    })
+    .then((res) => {
+        console.log(res);
+        return jaci.select("What do you like ", { options: { rabbit: "R", cat: "C" } })
+    })
+    .then((res) => {
+        console.log(res);
+        jaci.done();
+        return;
+    })
+    .catch((e) => {
+        console.log("error", e);
+        jaci.done();
+    })
